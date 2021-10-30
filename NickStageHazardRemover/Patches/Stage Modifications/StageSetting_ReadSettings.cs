200a86a166ce37b8fe575e04d1bd4a3ff5a0439a
Megalon <megalongaming@gmail.com>
2021-10-30 01:09:09 -0700

    Remove extra logging

--- NickStageHazardRemover/Patches/Stage Modifications/StageSetting_ReadSettings.cs
+++ NickStageHazardRemover/Patches/Stage Modifications/StageSetting_ReadSettings.cs
@@ -11,10 +11,8 @@ namespace NickStageHazardRemover.Patches.Stage_Modifications
     {
         static void Prefix(ref StageMetaData smd)
         {
-            Plugin.LogWarning($"smd.id:{smd.id}");
             if (smd.id.Equals("stage_mascot_blocks"))
             {
-                Plugin.LogWarning($"smd.blastzoneDistDown:{smd.blastzoneDistDown}");
                 smd.blastzoneDistDown = 15f;
             }
         }
