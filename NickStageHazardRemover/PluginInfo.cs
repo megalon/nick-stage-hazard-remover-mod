eaa6e2749ab04f17d743d40ebd740fef97f10ffd
Megalon <megalongaming@gmail.com>
2021-10-30 00:50:26 -0700

    Version bump

--- NickStageHazardRemover/PluginInfo.cs
+++ NickStageHazardRemover/PluginInfo.cs
@@ -8,6 +8,6 @@ namespace NickStageHazardRemover
     {
         public const string PLUGIN_GUID = "megalon.nick_stage_hazard_remover_mod";
         public const string PLUGIN_NAME = "NickStageHazardRemover";
-        public const string PLUGIN_VERSION = "0.1.0";
+        public const string PLUGIN_VERSION = "1.0.0";
     }
 }
