9766ca8de80a9436efa4e11eac9d37fe2969c94e
Megalon <megalongaming@gmail.com>
2021-10-30 01:07:17 -0700

    Move bottom blastzone down for mascot stage

---
+++ NickStageHazardRemover/Patches/Stage Modifications/StageSetting_ReadSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using Nick;
+
+namespace NickStageHazardRemover.Patches.Stage_Modifications
+{
+    [HarmonyPatch(typeof(GameSetup.StageSetting), "ReadSettings")]
+    class StageSetting_ReadSettings
+    {
+        static void Prefix(ref StageMetaData smd)
+        {
+            Plugin.LogWarning($"smd.id:{smd.id}");
+            if (smd.id.Equals("stage_mascot_blocks"))
+            {
+                Plugin.LogWarning($"smd.blastzoneDistDown:{smd.blastzoneDistDown}");
+                smd.blastzoneDistDown = 15f;
+            }
+        }
+    }
+}
